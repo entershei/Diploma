f56a9e469d034cb82b621a6bcc4a5793ac3f0735
Irina <entershei@gmail.com>
2021-01-28 03:39:09 +0700

    A and B compartments

--- Simulation_fragile_breakage_model/main.py
+++ Simulation_fragile_breakage_model/main.py
@@ -1,11 +1,13 @@
-import numpy as np
+import csv
+import random
+
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def read_number_of_regions_and_steps():
-    print('Enter number of regions (should be an even number)')
+    print('Enter number of fragile regions')
     n = int(input())
-    assert n % 2 == 0
 
     print('Enter number of steps')
     k = int(input())
@@ -14,91 +16,214 @@ def read_number_of_regions_and_steps():
 
 
 def read_probabilities():
-    print('Enter probability of rearrangement between fragile regions:')
-    p_ff = float(input())
+    print('Enter probability of rearrangement between A-type fragile regions:')
+    p_aa = float(input())
 
-    print('Enter probability of rearrangement between solid regions:')
-    p_ss = float(input())
+    print('Enter probability of rearrangement between B-type fragile regions:')
+    p_bb = float(input())
 
-    # Probability of rearrangement between fragile and solid regions.
-    p_fs = 1 - p_ss - p_ff
+    # Probability of rearrangement between A-type and D-type fragile regions.
+    p_ab = 1 - p_aa - p_bb
 
-    assert p_ff >= 0 and p_ss >= 0 and p_fs >= 0
+    assert p_aa >= 0 and p_bb >= 0 and p_ab >= 0
 
-    return p_ff, p_ss, p_fs
+    return p_aa, p_bb, p_ab
 
 
-def update_cycles(a, b, positions, cnt_cycles, colors, new_color):
-    if positions[a] == b:
-        a, b = b, a
+def is_in_cycle(x, edges, y):
+    cur = edges[x]
 
-    if colors[a] == colors[b]:
-        if positions[b] == a:
-            positions[b] = positions[a]
-            positions[a] = a
-            colors[a] = new_color
+    if y == cur:
+        return True
+
+    while cur != x:
+        if cur % 2 == 0:
+            cur = cur + 1
         else:
-            positions[a], positions[b] = positions[b], positions[a]
+            cur = cur - 1
 
-            colors[a] = new_color
-            cur = positions[a]
-            while cur != a:
-                colors[cur] = new_color
-                cur = positions[cur]
-        return cnt_cycles + 1, new_color + 1
+        if y == cur:
+            return True
+
+        if cur == x:
+            break
+
+        cur = edges[cur]
+
+        if y == cur:
+            return True
+
+    return False
+
+
+def set_color(x, edges, colors, color):
+    colors[x] = color
+    cur = edges[x]
+
+    while cur != x:
+        colors[cur] = color
+
+        if cur % 2 == 0:
+            cur = cur + 1
+        else:
+            cur = cur - 1
+
+        if cur == x:
+            break
+
+        colors[cur] = color
+        cur = edges[cur]
+
+
+def update_cycles_number(edge1, edge2, joining_type, edges, cnt_cycles, colors, new_color):
+    x, y = edge1
+    u, v = edge2
+
+    if joining_type == 'xu':
+        edges[x], edges[u] = u, x
+        edges[y], edges[v] = v, y
     else:
-        positions[a], positions[b] = positions[b], positions[a]
-        colors[b] = colors[a]
-        cur = positions[b]
-        while cur != b:
-            colors[cur] = colors[b]
-            cur = positions[cur]
+        edges[x], edges[v] = v, x
+        edges[y], edges[u] = u, y
 
+    if colors[x] == colors[u]:
+        if is_in_cycle(x, edges, y):
+            return cnt_cycles, new_color
+        else:
+            set_color(x, edges, colors, new_color)
+            return cnt_cycles + 1, new_color + 1
+    else:
+        set_color(x, edges, colors, colors[x])
         return cnt_cycles - 1, new_color
 
 
-def markov_process(n, k, p_ff, p_ss, p_fs, f_log):
-    positions = list(range(-1, n))
-    probabilities = [p_ff, p_ss, p_fs]
+def update_edges(edge1, edge2, join_type):
+    x, y = edge1
+    u, v = edge2
+
+    if join_type == 'xu':
+        new_edges = [[x, u], [y, v]]
+    else:
+        new_edges = [[x, v], [y, u]]
+
+    random.shuffle(new_edges)
+    return new_edges
+
+
+def compute_cycles_lens(n, k, colors, log_lens):
+    cycles = {}
+    for c in colors:
+        if c in cycles.keys():
+            cycles[c] += 1
+        else:
+            cycles[c] = 1
+
+    lens = [0] * 6
+    max_len = 0
+    for c in cycles.keys():
+        cur_len = cycles[c] // 2
+        if cur_len < 6:
+            lens[cur_len] += 1
+        max_len = max(max_len, cur_len)
+
+    log_lens.writerow(
+        {'n': n, 'k': k, 'cycles': len(cycles), '1-cycles': lens[1], '2-cycles': lens[2], '3-cycles': lens[3],
+         '4-cycles': lens[4], '5-cycles': lens[5], 'max_cycle_len': max_len})
+
+
+def markov_process(n, k, p_aa, p_bb, p_ab, a_type, b_type, edges, f_log, log_lens):
+    len_a = len(a_type)
+    len_b = len(b_type)
+
+    probabilities = [p_aa, p_bb, p_ab]
 
     cycles = [n]
-    colors = list(range(n))
+    colors = [0] * (2 * n)
+    for i in range(n):
+        colors[2 * i] = i
+        colors[2 * i + 1] = i
+
     cnt_cycles = n
-    v_out = list(range(n))
     new_color = n
 
+    cnt_aa = 0
+    cnt_bb = 0
+    cnt_ab = 0
+
+    compute_cycles_lens(n, 0, colors, log_lens)
+
     for i in range(k):
-        p = np.random.choice(3, 1, p=probabilities)
+        dcj_type = np.random.choice(['aa', 'bb', 'ab'], 1, p=probabilities)[0]
+        joining_type = np.random.choice(['xu', 'xv'], 1)[0]
 
-        if p == 0:
-            a = np.random.randint(1, n // 2)
-            a = a * 2 - 1
-            b = np.random.randint(1, n // 2)
-            b = b * 2 - 1
-            regions = 'ff'
-        elif p == 1:
-            a = np.random.randint(1, n // 2)
-            a = a * 2
-            b = np.random.randint(1, n // 2)
-            b = b * 2
-            regions = 'ss'
+        if dcj_type == 'aa':
+            cnt_aa += 1
+            edge_ind1, edge_ind2 = np.random.choice(len(a_type), 2, replace=False)
+            edge1 = a_type[edge_ind1]
+            edge2 = a_type[edge_ind2]
+
+            cnt_cycles, new_color = update_cycles_number(edge1, edge2, joining_type, edges, cnt_cycles, colors,
+                                                         new_color)
+
+            new_edges = update_edges(edge1, edge2, joining_type)
+            a_type[edge_ind1], a_type[edge_ind2] = new_edges
+        elif dcj_type == 'bb':
+            cnt_bb += 1
+            edge_ind1, edge_ind2 = np.random.choice(len(b_type), 2, replace=False)
+            edge1 = b_type[edge_ind1]
+            edge2 = b_type[edge_ind2]
+
+            cnt_cycles, new_color = update_cycles_number(edge1, edge2, joining_type, edges, cnt_cycles, colors,
+                                                         new_color)
+
+            new_edges = update_edges(edge1, edge2, joining_type)
+            b_type[edge_ind1], b_type[edge_ind2] = new_edges
         else:
-            a = np.random.randint(1, n // 2)
-            a = a * 2 - 1
-            b = np.random.randint(1, n // 2)
-            b = b * 2
-            regions = 'fs'
+            cnt_ab += 1
+            edge_ind1 = np.random.choice(len(a_type), 1)[0]
+            edge_ind2 = np.random.choice(len(b_type), 1)[0]
 
-        cnt_cycles, new_color = update_cycles(a, b, v_out, cnt_cycles, colors, new_color)
+            edge1 = a_type[edge_ind1]
+            edge2 = b_type[edge_ind2]
+
+            cnt_cycles, new_color = update_cycles_number(edge1, edge2, joining_type, edges, cnt_cycles, colors,
+                                                         new_color)
+
+            new_edges = update_edges(edge1, edge2, joining_type)
+            a_type[edge_ind1], b_type[edge_ind2] = new_edges
+
         cycles.append(cnt_cycles)
 
-        # f_log.write('Regions = ' + regions + ' ' + str(a) + ' ' + str(b) + '\n')
-        # f_log.write('Number of cycles = ' + str(cnt_cycles) + '\n')
+        assert len_a == len(a_type) and len_b == len(b_type)
 
-        # f_log.write('Positions:\n')
-        # for position in positions:
-        #     f_log.write(str(position) + ' ')
-        # f_log.write('\n\n')
+        if i < 10:
+            f_log.write(
+                'Step ' + str(i) + ':\nRegions = ' + dcj_type + ' ' + joining_type + ' (' + str(edge1[0]) + ', ' + str(
+                    edge1[1]) + ') ('
+                + str(edge2[0]) + ', ' + str(edge2[1]) + ')    ')
+            f_log.write('Number of cycles = ' + str(cnt_cycles) + '\n')
+
+            # f_log.write('Edges:\n')
+            # for e in edges:
+            #     f_log.write(str(e) + ' ')
+            #
+            # f_log.write('\nA-type:\n')
+            # for e in a_type:
+            #     f_log.write('(' + str(e[0]) + ', ' + str(e[1]) + ')  ')
+            #
+            # f_log.write('\nB-type:\n')
+            # for e in b_type:
+            #     f_log.write('(' + str(e[0]) + ', ' + str(e[1]) + ')  ')
+            #
+            # f_log.write('\nColors:\n')
+            # for c in colors:
+            #     f_log.write(str(c) + ' ')
+
+            f_log.write('\n\n')
+
+        compute_cycles_lens(n, i + 1, colors, log_lens)
+
+    f_log.write('\nNumber aa, bb, ab = ' + str(cnt_aa) + ' ' + str(cnt_bb) + ' ' + str(cnt_ab) + '\n')
 
     return cycles
 
@@ -113,14 +238,43 @@ def draw_number_of_cycles(cycles):
     plt.show()
 
 
+def split_fragile_edges(n, p):
+    a_type = []
+    b_type = []
+    edges = [0] * (2 * n)
+
+    for i in range(n):
+        a_or_b = np.random.choice(['a', 'b'], 1, p=p)[0]
+        edge = [2 * i, 2 * i + 1]
+        if a_or_b == 'a':
+            a_type.append(edge)
+        else:
+            b_type.append(edge)
+        edges[2 * i] = 2 * i + 1
+        edges[2 * i + 1] = 2 * i
+
+    return a_type, b_type, edges
+
+
 def main():
     # n, k = read_number_of_regions_and_steps()
-    # p_ff, p_ss, p_fs = read_probabilities()
-    n, k, p_ff, p_ss, p_fs = 10000, 100000, 0.8, 0.15, 0.05
+    # p_aa, p_bb, p_ab = read_probabilities()
+    n, p_aa, p_bb, p_ab = 2000, 0.5, 0.45, 0.05
+    k = n * 2
 
-    f_log = open('log', 'w')
-    cycles = markov_process(n, k, p_ff, p_ss, p_fs, f_log)
-    draw_number_of_cycles(cycles)
+    p_split_a_b = [0.5, 0.5]
+    a_type, b_type, edges = split_fragile_edges(n, p_split_a_b)
+
+    f_log = open('log.txt', 'w')
+
+    with open('log_lens.csv', 'w', newline='') as f_log_lens:
+        fieldnames = ['n', 'k', 'cycles', '1-cycles', '2-cycles', '3-cycles', '4-cycles', '5-cycles',
+                      'max_cycle_len']
+        log_lens = csv.DictWriter(f_log_lens, fieldnames=fieldnames)
+        log_lens.writeheader()
+
+        cycles = markov_process(n, k, p_aa, p_bb, p_ab, a_type, b_type, edges, f_log, log_lens)
+        draw_number_of_cycles(cycles)
 
 
 if __name__ == '__main__':
