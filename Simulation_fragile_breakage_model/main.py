4612fcd75d2be244d6f019d20348466b57183303
Irina <entershei@gmail.com>
2021-03-09 03:08:11 +0700

    add aggregated results about cycles in 50_20 experiments

--- Simulation_fragile_breakage_model/main.py
+++ Simulation_fragile_breakage_model/main.py
@@ -5,20 +5,20 @@ import numpy as np
 
 
 def read_number_of_fragile_regions_and_steps():
-    print('Enter number of fragile regions')
+    print("Enter number of fragile regions")
     n = int(input())
 
-    print('Enter number of steps')
+    print("Enter number of steps")
     k = int(input())
 
     return n, k
 
 
 def read_probabilities():
-    print('Enter probability of rearrangement between A-type fragile regions:')
+    print("Enter probability of rearrangement between A-type fragile regions:")
     p_aa = float(input())
 
-    print('Enter probability of rearrangement between B-type fragile regions:')
+    print("Enter probability of rearrangement between B-type fragile regions:")
     p_bb = float(input())
 
     # Probability of rearrangement between A-type and D-type fragile regions.
@@ -31,7 +31,7 @@ def read_probabilities():
 
 class EdgeInfo:
     to = 0
-    edge_type = 'A'
+    edge_type = "A"
 
     def __init__(self, to, edge_type):
         self.to = to
@@ -43,7 +43,9 @@ def split_fragile_edges(n, a_type_edges_proportion):
     b_type = []
     edges = []
 
-    will_be_a_type = np.random.choice(range(0, n), int(a_type_edges_proportion * n), replace=False)
+    will_be_a_type = np.random.choice(
+        range(0, n), int(a_type_edges_proportion * n), replace=False
+    )
     is_a_type = [False] * n
     for e in will_be_a_type:
         is_a_type[e] = True
@@ -52,12 +54,12 @@ def split_fragile_edges(n, a_type_edges_proportion):
         edge = [2 * i, 2 * i + 1]
         if is_a_type[i]:
             a_type.append(edge)
-            edges.append(EdgeInfo(2 * i + 1, 'A'))
-            edges.append(EdgeInfo(2 * i, 'A'))
+            edges.append(EdgeInfo(2 * i + 1, "A"))
+            edges.append(EdgeInfo(2 * i, "A"))
         else:
             b_type.append(edge)
-            edges.append(EdgeInfo(2 * i + 1, 'B'))
-            edges.append(EdgeInfo(2 * i, 'B'))
+            edges.append(EdgeInfo(2 * i + 1, "B"))
+            edges.append(EdgeInfo(2 * i, "B"))
 
     return a_type, b_type, edges
 
@@ -111,23 +113,27 @@ def update_cycles(edge1, edge2, joining_type, edges, colors, last_color):
     x, y = edge1
     u, v = edge2
 
-    if joining_type == 'xu':
-        new_edge1_type, new_edge2_type = np.random.choice([edges[x].edge_type, edges[u].edge_type], 2, replace=False)
+    if joining_type == "xu":
+        new_edge1_type, new_edge2_type = np.random.choice(
+            [edges[x].edge_type, edges[u].edge_type], 2, replace=False
+        )
 
         edges[x], edges[u] = EdgeInfo(u, new_edge1_type), EdgeInfo(x, new_edge1_type)
         edges[y], edges[v] = EdgeInfo(v, new_edge2_type), EdgeInfo(y, new_edge2_type)
 
         new_edges = [[x, u], [y, v]]
-        if new_edge1_type != new_edge2_type and new_edge1_type == 'B':
+        if new_edge1_type != new_edge2_type and new_edge1_type == "B":
             new_edges.reverse()
     else:
-        new_edge1_type, new_edge2_type = np.random.choice([edges[x].edge_type, edges[v].edge_type], 2, replace=False)
+        new_edge1_type, new_edge2_type = np.random.choice(
+            [edges[x].edge_type, edges[v].edge_type], 2, replace=False
+        )
 
         edges[x], edges[v] = EdgeInfo(v, new_edge1_type), EdgeInfo(x, new_edge1_type)
         edges[y], edges[u] = EdgeInfo(u, new_edge2_type), EdgeInfo(y, new_edge2_type)
 
         new_edges = [[x, v], [y, u]]
-        if new_edge1_type != new_edge2_type and new_edge1_type == 'B':
+        if new_edge1_type != new_edge2_type and new_edge1_type == "B":
             new_edges.reverse()
 
     if colors[x] == colors[u]:
@@ -213,22 +219,26 @@ def markov_process(n, k, p_aa, p_bb, p_ab, a_type, b_type, edges):
     steps_cycles_info.append(compute_cycles_info(colors, edges))
 
     for i in range(k):
-        dcj_type = np.random.choice(['aa', 'bb', 'ab'], 1, p=probabilities)[0]
-        joining_type = np.random.choice(['xu', 'xv'], 1)[0]
+        dcj_type = np.random.choice(["aa", "bb", "ab"], 1, p=probabilities)[0]
+        joining_type = np.random.choice(["xu", "xv"], 1)[0]
 
-        if dcj_type == 'aa':
+        if dcj_type == "aa":
             edge_ind1, edge_ind2 = np.random.choice(len(a_type), 2, replace=False)
             edge1 = a_type[edge_ind1]
             edge2 = a_type[edge_ind2]
 
-            new_color, new_edges = update_cycles(edge1, edge2, joining_type, edges, colors, new_color)
+            new_color, new_edges = update_cycles(
+                edge1, edge2, joining_type, edges, colors, new_color
+            )
             a_type[edge_ind1], a_type[edge_ind2] = new_edges
-        elif dcj_type == 'bb':
+        elif dcj_type == "bb":
             edge_ind1, edge_ind2 = np.random.choice(len(b_type), 2, replace=False)
             edge1 = b_type[edge_ind1]
             edge2 = b_type[edge_ind2]
 
-            new_color, new_edges = update_cycles(edge1, edge2, joining_type, edges, colors, new_color)
+            new_color, new_edges = update_cycles(
+                edge1, edge2, joining_type, edges, colors, new_color
+            )
             b_type[edge_ind1], b_type[edge_ind2] = new_edges
         else:
             edge_ind1 = np.random.choice(len(a_type), 1)[0]
@@ -237,7 +247,9 @@ def markov_process(n, k, p_aa, p_bb, p_ab, a_type, b_type, edges):
             edge1 = a_type[edge_ind1]
             edge2 = b_type[edge_ind2]
 
-            new_color, new_edges = update_cycles(edge1, edge2, joining_type, edges, colors, new_color)
+            new_color, new_edges = update_cycles(
+                edge1, edge2, joining_type, edges, colors, new_color
+            )
             a_type[edge_ind1], b_type[edge_ind2] = new_edges
 
         steps_cycles_info.append(compute_cycles_info(colors, edges))
@@ -254,15 +266,36 @@ def aggregate_cycles_info(experiments):
     aggregated_cycles_info = []
     for i in range(num_steps_of_markov_process):
         average_all_cycles_num = 0
-        average_cnt_cycles = {'A': 0, 'B': 0, 'AA': 0, 'AB': 0, 'BB': 0, 'AAA': 0, 'AAB': 0, 'ABB': 0, 'BBB': 0,
-                              'AAAA': 0, 'AAAB': 0, 'AABB': 0, 'ABBB': 0, 'BBBB': 0,
-                              'AAAAA': 0, 'AAAAB': 0, 'AAABB': 0, 'AABBB': 0, 'ABBBB': 0, 'BBBBB': 0}
+        average_cnt_cycles = {
+            "A": 0,
+            "B": 0,
+            "AA": 0,
+            "AB": 0,
+            "BB": 0,
+            "AAA": 0,
+            "AAB": 0,
+            "ABB": 0,
+            "BBB": 0,
+            "AAAA": 0,
+            "AAAB": 0,
+            "AABB": 0,
+            "ABBB": 0,
+            "BBBB": 0,
+            "AAAAA": 0,
+            "AAAAB": 0,
+            "AAABB": 0,
+            "AABBB": 0,
+            "ABBBB": 0,
+            "BBBBB": 0,
+        }
 
         average_max_cycles_len = 0
         for j in range(num_experiments):
             average_all_cycles_num += experiments[j][i].num_all_cycles
             for cycles_types in experiments[j][i].num_n_cycles.keys():
-                average_cnt_cycles[cycles_types] += experiments[j][i].num_n_cycles[cycles_types]
+                average_cnt_cycles[cycles_types] += experiments[j][i].num_n_cycles[
+                    cycles_types
+                ]
 
             average_max_cycles_len += experiments[j][i].max_len
 
@@ -271,64 +304,114 @@ def aggregate_cycles_info(experiments):
             average_cnt_cycles[cycles_types] /= num_experiments
         average_max_cycles_len /= num_experiments
 
-        aggregated_cycles_info.append(CyclesInfo(average_all_cycles_num, average_cnt_cycles, average_max_cycles_len))
+        aggregated_cycles_info.append(
+            CyclesInfo(
+                average_all_cycles_num, average_cnt_cycles, average_max_cycles_len
+            )
+        )
 
     return aggregated_cycles_info
 
 
 def log_aggregated_results(n, aggregated_cycles_info, f):
-    with open(f, 'w', newline='') as f_log_lens:
-        fieldnames = ['n', 'k', 'all-cycles', 'A-cycles', 'B-cycles', 'AA-cycles', 'AB-cycles', 'BB-cycles',
-                      'AAA-cycles', 'AAB-cycles', 'ABB-cycles', 'BBB-cycles',
-                      'AAAA-cycles', 'AAAB-cycles', 'AABB-cycles', 'ABBB-cycles', 'BBBB-cycles',
-                      'AAAAA-cycles', 'AAAAB-cycles', 'AAABB-cycles', 'AABBB-cycles', 'ABBBB-cycles', 'BBBBB-cycles',
-                      'max_cycle_len']
+    with open(f, "w", newline="") as f_log_lens:
+        fieldnames = [
+            "n",
+            "k",
+            "all-cycles",
+            "A-cycles",
+            "B-cycles",
+            "AA-cycles",
+            "AB-cycles",
+            "BB-cycles",
+            "AAA-cycles",
+            "AAB-cycles",
+            "ABB-cycles",
+            "BBB-cycles",
+            "AAAA-cycles",
+            "AAAB-cycles",
+            "AABB-cycles",
+            "ABBB-cycles",
+            "BBBB-cycles",
+            "AAAAA-cycles",
+            "AAAAB-cycles",
+            "AAABB-cycles",
+            "AABBB-cycles",
+            "ABBBB-cycles",
+            "BBBBB-cycles",
+            "max_cycle_len",
+        ]
         log_cycles_info = csv.DictWriter(f_log_lens, fieldnames=fieldnames)
         log_cycles_info.writeheader()
 
         for step, info in enumerate(aggregated_cycles_info):
-            log_cycles_info.writerow({'n': n, 'k': step, 'all-cycles': info.num_all_cycles,
-                                      'A-cycles': info.num_n_cycles['A'], 'B-cycles': info.num_n_cycles['B'],
-                                      'AA-cycles': info.num_n_cycles['AA'], 'AB-cycles': info.num_n_cycles['AB'],
-                                      'BB-cycles': info.num_n_cycles['BB'],
-                                      'AAA-cycles': info.num_n_cycles['AAA'], 'AAB-cycles': info.num_n_cycles['AAB'],
-                                      'ABB-cycles': info.num_n_cycles['ABB'], 'BBB-cycles': info.num_n_cycles['BBB'],
-                                      'AAAA-cycles': info.num_n_cycles['AAAA'],
-                                      'AAAB-cycles': info.num_n_cycles['AAAB'],
-                                      'AABB-cycles': info.num_n_cycles['AABB'],
-                                      'ABBB-cycles': info.num_n_cycles['ABBB'],
-                                      'BBBB-cycles': info.num_n_cycles['BBBB'],
-                                      'AAAAA-cycles': info.num_n_cycles['AAAAA'],
-                                      'AAAAB-cycles': info.num_n_cycles['AAAAB'],
-                                      'AAABB-cycles': info.num_n_cycles['AAABB'],
-                                      'AABBB-cycles': info.num_n_cycles['AABBB'],
-                                      'ABBBB-cycles': info.num_n_cycles['ABBBB'],
-                                      'BBBBB-cycles': info.num_n_cycles['BBBBB'],
-                                      'max_cycle_len': info.max_len})
+            log_cycles_info.writerow(
+                {
+                    "n": n,
+                    "k": step,
+                    "all-cycles": info.num_all_cycles,
+                    "A-cycles": info.num_n_cycles["A"],
+                    "B-cycles": info.num_n_cycles["B"],
+                    "AA-cycles": info.num_n_cycles["AA"],
+                    "AB-cycles": info.num_n_cycles["AB"],
+                    "BB-cycles": info.num_n_cycles["BB"],
+                    "AAA-cycles": info.num_n_cycles["AAA"],
+                    "AAB-cycles": info.num_n_cycles["AAB"],
+                    "ABB-cycles": info.num_n_cycles["ABB"],
+                    "BBB-cycles": info.num_n_cycles["BBB"],
+                    "AAAA-cycles": info.num_n_cycles["AAAA"],
+                    "AAAB-cycles": info.num_n_cycles["AAAB"],
+                    "AABB-cycles": info.num_n_cycles["AABB"],
+                    "ABBB-cycles": info.num_n_cycles["ABBB"],
+                    "BBBB-cycles": info.num_n_cycles["BBBB"],
+                    "AAAAA-cycles": info.num_n_cycles["AAAAA"],
+                    "AAAAB-cycles": info.num_n_cycles["AAAAB"],
+                    "AAABB-cycles": info.num_n_cycles["AAABB"],
+                    "AABBB-cycles": info.num_n_cycles["AABBB"],
+                    "ABBBB-cycles": info.num_n_cycles["ABBBB"],
+                    "BBBBB-cycles": info.num_n_cycles["BBBBB"],
+                    "max_cycle_len": info.max_len,
+                }
+            )
 
 
 def main():
     # n, k = read_number_of_regions_and_steps()
     # p_aa, p_bb, p_ab = read_probabilities()
     start_time = time.time()
-    n, p_aa, p_bb = 1000, 0.5, 0.45
-    p_ab = 1 - p_aa - p_bb
-    k = n * 2
+    n, p_aa, p_bb = 1000, 0.45, 0.45
     a_type_edges_proportion = 0.5
 
+    p_ab = 1 - p_aa - p_bb
+    k = n * 2
+
     experiments = []
-    for i in range(20):
+    different_fragile_edges_splits = 50
+    markov_process_experiments = 20
+    for i in range(different_fragile_edges_splits):
         a_type, b_type, edges = split_fragile_edges(n, a_type_edges_proportion)
-        for j in range(10):
-            experiments.append(markov_process(n, k, p_aa, p_bb, p_ab, a_type.copy(), b_type.copy(), edges.copy()))
+        for j in range(markov_process_experiments):
+            experiments.append(
+                markov_process(
+                    n, k, p_aa, p_bb, p_ab, a_type.copy(), b_type.copy(), edges.copy()
+                )
+            )
 
     aggregated_cycles_info = aggregate_cycles_info(experiments)
 
-    log_aggregated_results(n, aggregated_cycles_info,
-                           'logs/cycles_info/n1000/20_10_experiments/' + 'paa0_5_pbb0_45_alpha0_5.csv')
+    log_aggregated_results(
+        n,
+        aggregated_cycles_info,
+        "logs/cycles_info/n1000/"
+        + str(different_fragile_edges_splits)
+        + "_"
+        + str(markov_process_experiments)
+        + "_experiments/"
+        + "paa0_45_pbb0_45_alpha0_5.csv",
+    )
     end_time = time.time()
     print(end_time - start_time)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
