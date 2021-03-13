9b11d81482550a15272592c07d99173a30aa4e5d
Irina <entershei@gmail.com>
2021-03-14 00:16:11 +0700

    add result comparison for 50_20 experiments

--- Simulation_fragile_breakage_model/results_comparison.py
+++ Simulation_fragile_breakage_model/results_comparison.py
@@ -6,12 +6,12 @@ import matplotlib.pyplot as plt
 
 def read_logs(f, cycles_types):
     num_c_n = []
-    with open(f, newline='') as csvfile:
-        reader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
+    with open(f, newline="") as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=",", quotechar="|")
         i = 0
         for row in reader:
-            n = int(row['n'])
-            k = int(row['k'])
+            n = int(row["n"])
+            k = int(row["k"])
             assert i == k
 
             c_n = {}
@@ -27,7 +27,7 @@ def compute_analytical_c1(x, p_aa, p_bb, alpha):
     beta = 1 - alpha
     a_cycles = alpha * math.exp(-1 * x * (1 + p_aa - p_bb) / alpha)
     b_cycles = beta * math.exp(-1 * x * (1 + p_bb - p_aa) / beta)
-    return {'A-cycles': a_cycles, 'B-cycles': b_cycles, 'all': a_cycles + b_cycles}
+    return {"A-cycles": a_cycles, "B-cycles": b_cycles, "all": a_cycles + b_cycles}
 
 
 def compute_analytical_c2(x, p_aa, p_bb, alpha):
@@ -35,16 +35,66 @@ def compute_analytical_c2(x, p_aa, p_bb, alpha):
     p_ab = 1 - p_aa - p_bb
     aa_cycles = p_aa * x * math.exp(-2 * x * (1 + p_aa - p_bb) / alpha)
     bb_cycles = p_bb * x * math.exp(-2 * x * (1 + p_bb - p_aa) / beta)
-    ab_cycles = p_ab * x * math.exp(-1 * x * (2 * alpha * p_bb + alpha * p_ab + 2 * beta * p_aa + beta * p_ab)
-                                    / (alpha * beta))
+    ab_cycles = (
+        p_ab
+        * x
+        * math.exp(
+            -1
+            * x
+            * (2 * alpha * p_bb + alpha * p_ab + 2 * beta * p_aa + beta * p_ab)
+            / (alpha * beta)
+        )
+    )
 
-    return {'AA-cycles': aa_cycles, 'AB-cycles': ab_cycles, 'BB-cycles': bb_cycles,
-            'all': aa_cycles + ab_cycles + bb_cycles}
+    return {
+        "AA-cycles": aa_cycles,
+        "AB-cycles": ab_cycles,
+        "BB-cycles": bb_cycles,
+        "all": aa_cycles + ab_cycles + bb_cycles,
+    }
+
+
+def compute_analytical_c3(x, p_aa, p_bb, alpha):
+    beta = 1 - alpha
+    p_ab = 1 - p_aa - p_bb
+
+    aaa_cycles = (
+        2 * (p_aa ** 2) * x ** 2 / alpha * math.exp(-3 * x * (2 * p_aa + p_ab) / alpha)
+    )
+    aab_cycles = (
+        2 * x ** 2 * p_aa * p_ab / alpha + (p_ab * x) ** 2 / (2 * beta)
+    ) * math.exp(
+        -1
+        * x
+        * (2 * alpha * p_bb + alpha * p_ab + 4 * beta * p_aa + 2 * beta * p_ab)
+        / (alpha * beta)
+    )
+    abb_cycles = (
+        2 * x ** 2 * p_bb * p_ab / beta + (p_ab * x) ** 2 / (2 * alpha)
+    ) * math.exp(
+        -1
+        * x
+        * (2 * beta * p_aa + beta * p_ab + 4 * alpha * p_bb + 2 * alpha * p_ab)
+        / (alpha * beta)
+    )
+    bbb_cycles = (
+        2 * (p_bb ** 2) * x ** 2 / beta * math.exp(-3 * x * (2 * p_bb + p_ab) / beta)
+    )
+
+    return {
+        "AAA-cycles": aaa_cycles,
+        "AAB-cycles": aab_cycles,
+        "ABB-cycles": abb_cycles,
+        "BBB-cycles": bbb_cycles,
+        "all": aaa_cycles + aab_cycles + abb_cycles + bbb_cycles,
+    }
 
 
 # returns 100 * |analytical_c_n - real_c_n| / real_c_n
 # alpha = t / n, t is number of A-type edges
-def compute_relative_errors(x, real_c_ns, cycles_types, n, p_aa, p_bb, alpha, estimation_func):
+def compute_relative_errors(
+    x, real_c_ns, cycles_types, n, p_aa, p_bb, alpha, estimation_func
+):
     k = int(x * n)
 
     relative_errors = {}
@@ -58,27 +108,34 @@ def compute_relative_errors(x, real_c_ns, cycles_types, n, p_aa, p_bb, alpha, es
 
     for cycle_type in cycles_types:
         if real_c_n[cycle_type] != 0:
-            relative_errors[cycle_type] = 100 * abs(analytical_c_n[cycle_type] - real_c_n[cycle_type])\
-                                          / real_c_n[cycle_type]
+            relative_errors[cycle_type] = (
+                100
+                * abs(analytical_c_n[cycle_type] - real_c_n[cycle_type])
+                / real_c_n[cycle_type]
+            )
         else:
             relative_errors[cycle_type] = 0
-    relative_errors['all'] = 100 * abs(analytical_c_n['all'] - all_real_c_n) / all_real_c_n
+    relative_errors["all"] = (
+        100 * abs(analytical_c_n["all"] - all_real_c_n) / all_real_c_n
+    )
 
     return relative_errors
 
 
 def write_relative_error(error_depends_on_x, f, field_names):
-    with open(f, 'w', newline='') as log:
-        fieldnames = ['x'] + field_names
+    with open(f, "w", newline="") as log:
+        fieldnames = ["x"] + field_names
         log_lens = csv.DictWriter(log, fieldnames=fieldnames)
         log_lens.writeheader()
 
         for x in error_depends_on_x.keys():
-            error_depends_on_x[x]['x'] = x
+            error_depends_on_x[x]["x"] = x
             log_lens.writerow(error_depends_on_x[x])
 
 
-def compare_n_cycles(f_in, f_out, p_aa, p_bb, alpha, cycles_types, compute_analytical_c_n):
+def compare_n_cycles(
+    f_in, f_out, p_aa, p_bb, alpha, cycles_types, compute_analytical_c_n
+):
     real_c_n_s, n = read_logs(f_in, cycles_types)
 
     # x = k / n
@@ -86,34 +143,37 @@ def compare_n_cycles(f_in, f_out, p_aa, p_bb, alpha, cycles_types, compute_analy
     x = 0.05
     step = 0.05
     while x < 2.05:
-        error_depends_on_x[x] = compute_relative_errors(x, real_c_n_s, cycles_types, n, p_aa, p_bb, alpha,
-                                                        compute_analytical_c_n)
+        error_depends_on_x[x] = compute_relative_errors(
+            x, real_c_n_s, cycles_types, n, p_aa, p_bb, alpha, compute_analytical_c_n
+        )
         x += step
 
-    write_relative_error(error_depends_on_x, f_out, cycles_types + ['all'])
+    write_relative_error(error_depends_on_x, f_out, cycles_types + ["all"])
 
 
 def draw_average(real_e, ess_e, xs):
     plt.plot(xs, real_e)
     plt.plot(xs, ess_e)
-    plt.xlabel('Number of swaps')
-    plt.ylabel('E')
+    plt.xlabel("Number of swaps")
+    plt.ylabel("E")
     plt.grid()
     plt.show()
 
 
 def write_analytical_cycles(analytical_cycles_depends_on_x, f, field_names):
-    with open(f, 'w', newline='') as log:
-        fieldnames = field_names + ['x']
+    with open(f, "w", newline="") as log:
+        fieldnames = field_names + ["x"]
         log_lens = csv.DictWriter(log, fieldnames=fieldnames)
         log_lens.writeheader()
 
         for x in analytical_cycles_depends_on_x.keys():
-            analytical_cycles_depends_on_x[x]['x'] = x
+            analytical_cycles_depends_on_x[x]["x"] = x
             log_lens.writerow(analytical_cycles_depends_on_x[x])
 
 
-def compute_analytical_cycles(p_aa, p_bb, alpha, compute_analytical_c_n, f_out, field_names):
+def compute_analytical_cycles(
+    p_aa, p_bb, alpha, compute_analytical_c_n, f_out, field_names
+):
     # x = k / n
     analytical_cycles_depends_on_x = {}
     x = 0.05
@@ -126,32 +186,69 @@ def compute_analytical_cycles(p_aa, p_bb, alpha, compute_analytical_c_n, f_out, 
     write_analytical_cycles(analytical_cycles_depends_on_x, f_out, field_names)
 
 
+def generate_field_names(n):
+    cycle_types = []
+    for i in range(n + 1):
+        cycle_types.append("A" * (n - i) + "B" * i)
+    return cycle_types
+
+
+def result_comparison(
+    cycles, file_end, p_aa, p_bb, alpha, experiments, compute_analytical_c_n
+):
+    field_names = generate_field_names(cycles)
+    for i in range(len(field_names)):
+        field_names[i] += "-cycles"
+
+    compare_n_cycles(
+        f_in="logs/cycles_info/" + experiments + file_end,
+        f_out="logs/relative_error/"
+        + experiments
+        + str(cycles)
+        + "cycles/depends_on_x_"
+        + file_end,
+        p_aa=p_aa,
+        p_bb=p_bb,
+        alpha=alpha,
+        cycles_types=field_names,
+        compute_analytical_c_n=compute_analytical_c_n,
+    )
+
+    compute_analytical_cycles(
+        p_aa=p_aa,
+        p_bb=p_bb,
+        alpha=alpha,
+        compute_analytical_c_n=compute_analytical_c_n,
+        f_out="logs/analytical_cycles/n1000/c" + str(cycles) + "/" + file_end,
+        field_names=field_names + ["all"],
+    )
+
+
 def main():
-    experiments = 'n1000/20_10_experiments/'
+    experiments = "n1000/50_20_experiments/"
 
-    file_end = 'paa0_5_pbb0_45_alpha0_5.csv'
-    p_aa = 0.5
-    p_bb = 0.45
-    alpha = 0.5
+    different_parameters = [
+        ("paa0_2_pbb0_6_alpha0_9.csv", 0.2, 0.6, 0.9),
+        ("paa0_4_pbb0_35_alpha0_7.csv", 0.4, 0.35, 0.7),
+        ("paa0_5_pbb0_5_alpha0_5.csv", 0.5, 0.5, 0.5),
+        ("paa0_5_pbb0_5_alpha0_8.csv", 0.5, 0.5, 0.8),
+        ("paa0_5_pbb0_45_alpha0_5.csv", 0.5, 0.45, 0.5),
+        ("paa0_45_pbb0_45_alpha0_5.csv", 0.45, 0.45, 0.5),
+    ]
 
-    field_names1 = ['A-cycles', 'B-cycles']
-    compare_n_cycles(f_in='logs/cycles_info/' + experiments + file_end,
-                     f_out='logs/relative_error/' + experiments + '1cycles/depends_on_x_' + file_end,
-                     p_aa=p_aa, p_bb=p_bb, alpha=alpha, cycles_types=field_names1,
-                     compute_analytical_c_n=compute_analytical_c1)
+    for cur_parameters in different_parameters:
+        file_end, p_aa, p_bb, alpha = cur_parameters
 
-    compute_analytical_cycles(p_aa=p_aa, p_bb=p_bb, alpha=alpha, compute_analytical_c_n=compute_analytical_c1,
-                              f_out='logs/analytical_cycles/n1000/c1/' + file_end, field_names=field_names1 + ['all'])
-
-    field_names2 = ['AA-cycles', 'AB-cycles', 'BB-cycles']
-    compare_n_cycles(f_in='logs/cycles_info/' + experiments + file_end,
-                     f_out='logs/relative_error/' + experiments + '2cycles/depends_on_x_' + file_end,
-                     p_aa=p_aa, p_bb=p_bb, alpha=alpha, cycles_types=field_names2,
-                     compute_analytical_c_n=compute_analytical_c2)
-
-    compute_analytical_cycles(p_aa=p_aa, p_bb=p_bb, alpha=alpha, compute_analytical_c_n=compute_analytical_c2,
-                              f_out='logs/analytical_cycles/n1000/c2/' + file_end, field_names=field_names2 + ['all'])
+        result_comparison(
+            1, file_end, p_aa, p_bb, alpha, experiments, compute_analytical_c1
+        )
+        result_comparison(
+            2, file_end, p_aa, p_bb, alpha, experiments, compute_analytical_c2
+        )
+        result_comparison(
+            3, file_end, p_aa, p_bb, alpha, experiments, compute_analytical_c3
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
