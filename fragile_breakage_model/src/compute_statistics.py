74d684958b21fe6860c7bc73f4e18cfd8450df1e
Irina <entershei@gmail.com>
2021-04-25 22:48:38 +0700

    add plots for true evolutionary distance

--- fragile_breakage_model/src/compute_statistics.py
+++ fragile_breakage_model/src/compute_statistics.py
@@ -1,12 +1,15 @@
 from results_comparison import compute_analytical_cycles_m
 from generate_directories_names import get_cycles_info_dir
+import matplotlib.pyplot as plt
 
 import parameters
 from aggregate_cycles_info import read_experiments_cycles_info
 from draw_plots import draw
+import numpy as np
 
 
-def compute_analytically_normalized_min_dist(x, p_aa, p_bb, alpha, max_m):
+# Normalized min distance
+def compute_analytically_d_n(x, p_aa, p_bb, alpha, max_m):
     dist = 1.0
 
     for m in range(1, max_m):
@@ -15,32 +18,51 @@ def compute_analytically_normalized_min_dist(x, p_aa, p_bb, alpha, max_m):
     return dist
 
 
-def compute_analytically_normalized_number_non_trivial_cycles(
-    x, p_aa, p_bb, alpha, max_m
-):
-    # b = 0.0
-    # for m in range(2, max_m):
-    #     b += m * compute_analytical_cycles_m(m, x, p_aa, p_bb, alpha)["all"]
-    # return b
+# For non trivial cycles normalized sum of cycles lens
+def compute_analytically_b_n(x, p_aa, p_bb, alpha):
     return 1 - compute_analytical_cycles_m(1, x, p_aa, p_bb, alpha)["all"]
 
 
-def compute_d_with_different_parameters(max_m):
-    p = list(map(lambda a: a / 10, range(1, 10)))
-    for p_aa in p:
-        for p_bb in p:
-            if p_aa + p_bb < 1:
-                for x in p:
-                    for alpha in p:
-                        d, iterations = compute_analytically_normalized_min_dist(
-                            x, p_aa, p_bb, alpha, max_m=max_m
-                        )
-                        print(d, iterations, p_aa, p_bb, x, alpha)
+#                 "real_true_dist": k,
+#                 "analytical_true_dist": analytical_true_dist,
+#                 "real_min_dist": empirical_min_d,
+def draw_dists(to_draw, save_as):
+    xs = list(map(lambda v: v["real_true_dist"], to_draw))
+    empirical_true_distances = xs
+    empirical_min_distances = list(map(lambda v: v["real_min_dist"], to_draw))
+    analytical_true_distances = list(map(lambda v: v["analytical_true_dist"], to_draw))
 
-    # check_d_b(get_parameters_as_string() + "paa0,5_pbb0,45_alpha0,5_10000_experiments.csv")
+    legend = [
+        "Empirical true distance",
+        "Estimated true distance",
+        "Empirical min distance",
+    ]
+
+    plt.plot(xs, empirical_true_distances, color="blue", label=legend[0])
+    plt.plot(xs, analytical_true_distances, color="red", label=legend[1])
+    plt.plot(xs, empirical_min_distances, color="black", label=legend[2])
+
+    plt.title("Evolutionary distance")
+    plt.xlabel("Real number of rearrangements")
+    plt.ylabel("Estimated number of rearrangements")
+    plt.legend(legend)
+    plt.grid()
+    plt.savefig(save_as)
+    plt.close()
 
 
-def compare_d_and_b(parameters_str, p_aa, p_bb, a_type_edges_proportion, max_m):
+def find_true_evolution_dist(parameters_str, p_aa, p_bb, alpha, max_m):
+    def d_divide_by_b(possible_x):
+        analytical_min_d = compute_analytically_d_n(
+            possible_x,
+            p_aa,
+            p_bb,
+            alpha,
+            max_m,
+        )
+        analytical_sum_lens = compute_analytically_b_n(possible_x, p_aa, p_bb, alpha)
+        return analytical_min_d / analytical_sum_lens
+
     aggregated_info = read_experiments_cycles_info(
         get_cycles_info_dir() + parameters_str + ".csv",
         6,
@@ -48,39 +70,55 @@ def compare_d_and_b(parameters_str, p_aa, p_bb, a_type_edges_proportion, max_m):
         False,
     )[0]
 
+    print(parameters_str)
+
+    step = 5e-4
+    l = step
+    r = 1 + step
+
+    to_draw = []
     for k, info in enumerate(aggregated_info):
-        empirical_min_d = 0
+        if k == 0 or k % 5 != 0:
+            continue
+        print(k, "!")
+
+        empirical_d = 0
+        empirical_b = 0
         for cycle_len in info.cycles_m.keys():
             if int(cycle_len) > 1:
-                empirical_min_d += info.cycles_m[cycle_len] * (int(cycle_len) - 1)
+                empirical_d += info.cycles_m[cycle_len] * (int(cycle_len) - 1)
+                empirical_b += info.cycles_m[cycle_len] * int(cycle_len)
 
-        empirical_min_d /= parameters.NUMBER_OF_FRAGILE_EDGES
-        analytical_min_d = compute_analytically_normalized_min_dist(
-            k / parameters.NUMBER_OF_FRAGILE_EDGES,
-            p_aa,
-            p_bb,
-            a_type_edges_proportion,
-            max_m,
+        empirical_d_b = empirical_d / empirical_b
+
+        best_analytical_x = 0
+        min_error = 10000
+        for x in np.arange(l, r, step):
+            analytical_d_b = d_divide_by_b(x)
+            cur_error = abs(analytical_d_b - empirical_d_b)
+            if cur_error < min_error:
+                best_analytical_x, min_error = x, cur_error
+        analytical_b = compute_analytically_b_n(best_analytical_x, p_aa, p_bb, alpha)
+        analytical_n = empirical_b / analytical_b
+        analytical_true_dist = best_analytical_x * analytical_n
+
+        to_draw.append(
+            {
+                "real_true_dist": k,
+                "analytical_true_dist": analytical_true_dist,
+                "real_min_dist": empirical_d,
+            }
         )
-        # print(
-        #     k, abs(empirical_min_d - analytical_min_d), empirical_min_d, analytical_min_d
-        # )
-        if abs(empirical_min_d - analytical_min_d) <= 1e-2:
-            print(
-                k,
-                abs(empirical_min_d - analytical_min_d),
-                empirical_min_d,
-                analytical_min_d,
-            )
+
+    draw_dists(
+        to_draw,
+        "fragile_breakage_model/plots/true_evolution_distance/" + parameters_str,
+    )
 
 
 def compute_d_divide_b(x, p_aa, p_bb, alpha, max_m):
-    analytical_min_d = compute_analytically_normalized_min_dist(
-        x, p_aa, p_bb, alpha, max_m
-    )
-    b = compute_analytically_normalized_number_non_trivial_cycles(
-        x, p_aa, p_bb, alpha, max_m
-    )
+    analytical_min_d = compute_analytically_d_n(x, p_aa, p_bb, alpha, max_m)
+    b = compute_analytically_b_n(x, p_aa, p_bb, alpha)
     if b > 0:
         return analytical_min_d / b
     return 0
@@ -110,12 +148,12 @@ def check_monotone_d_divide_b():
 
 
 def compare_empirical_and_analytical():
-    parameter = parameters.PROBABILITIES_WITH_ALPHA[1]
+    parameter = parameters.PROBABILITIES_WITH_ALPHA[6]
     string_parameters, p_aa, p_bb, a_type_edges_proportion = parameter
-    compare_d_and_b(string_parameters, p_aa, p_bb, a_type_edges_proportion, 111)
+    find_true_evolution_dist(string_parameters, p_aa, p_bb, a_type_edges_proportion, 30)
 
 
 if __name__ == "__main__":
-    # compute_d_with_different_parameters()
     compare_empirical_and_analytical()
+    # check_monotone_d_divide_b()
     # check_monotone_d_divide_b()
